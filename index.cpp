c6a301ec7a01b67175b699de84629abc959963af
Vinícius Santos <viniciusssantos.pa@gmail.com>
2025-08-12 14:31:21 -0300

    feat menu options

--- index.cpp
+++ index.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 // 0. Disponibilizar um menu inicial que forneça as seguintes opções para o usuário : a.Cadastrar cidade;
@@ -11,10 +12,63 @@ using namespace std;
 // h.Contar Pokémons de cada tipo;
 // i.Encontrar Pokémons próximos;
 
+void cadastrarCidade()
+{
+    cout << "Cadastrar cidade em desenvolvimento" << endl;
+};
+
+void cadastrarEstrada()
+{
+    cout << "Cadastrar estrada em desenvolvimento" << endl;
+};
+
+void buscarCentroProximo()
+{
+    cout << "Buscar dinos em desenvolvimento" << endl;
+};
+
+void cadastrarDino()
+{
+    cout << "Cadastrar dinos em desenvolvimento" << endl;
+};
+
+void removerDino()
+{
+    cout << "Remoção de dinos em desenvolvimento" << endl;
+};
+
+void listarDinos(string filtragem)
+{
+    if (filtragem == "nome")
+    {
+        // Pelo Nome
+        cout << "Filtragem por nome em desenvolvimento" << endl;
+    }
+    else
+    {
+        // Pelo Tipo
+        cout << "Filtragem por tipo em desenvolvimento" << endl;
+    }
+};
+
+void contarDinos()
+{
+    cout << "Contagem de dinos em desenvolvimento" << endl;
+};
+
+void encontrarDinosProximos()
+{
+    cout << "Encontrar dinos proximos em desenvolvimento" << endl;
+};
+
+void sair()
+{
+    exit(0);
+}
+
 void menu()
 {
-    char choice; //Escolha
-
+    char choice; // Escolha
     cout << "Escolha uma dentre as opções abaixo:" << endl;
     cout << "" << endl;
     cout << "a.Cadastrar cidade" << endl;
@@ -26,31 +80,47 @@ void menu()
     cout << "g.Listar Pokémons(ordem alfabética de tipo)" << endl;
     cout << "h.Contar Pokémons de cada tipo" << endl;
     cout << "i.Encontrar Pokémons próximos" << endl;
+    cout << "j. Sair" << endl;
     cout << "" << endl;
 
-    cin >> choice; //Pega escolha do usuário
-
-    switch (choice) //Switch de acordo com a escolha do usuário
+    while (true)
     {
-    case 'a':
-        break;
-    case 'b':
-        break;
-    case 'c':
-        break;
-    case 'd':
-        break;
-    case 'e':
-        break;
-    case 'f':
-        break;
-    case 'g':
-        break;
-    case 'h':
-        break;
-    case 'i':
-        break;
-    };
+        cin >> choice; // Pega escolha do usuário
+
+        switch (choice) // Switch de acordo com a escolha do usuário
+        {
+        case 'a':
+            cadastrarCidade();
+            break;
+        case 'b':
+            cadastrarEstrada();
+            break;
+        case 'c':
+            buscarCentroProximo();
+            break;
+        case 'd':
+            cadastrarDino();
+            break;
+        case 'e':
+            removerDino();
+            break;
+        case 'f':
+            listarDinos("nome");
+            break;
+        case 'g':
+            listarDinos("tipo");
+            break;
+        case 'h':
+            contarDinos();
+            break;
+        case 'i':
+            encontrarDinosProximos();
+            break;
+        case 'j':
+            sair();
+            break;
+        }
+    }
 };
 
 int main()
